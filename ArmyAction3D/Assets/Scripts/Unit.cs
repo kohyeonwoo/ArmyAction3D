8b773d8d56a5aec4422832f177adbdd014d55ec9
kohyeonwoo <49010769+kohyeonwoo@users.noreply.github.com>
2025-10-30 11:20:33 +0900

    251030
    씬 오브젝트 반영 및 기타 내용 추가

---
+++ ArmyAction3D/Assets/Scripts/Unit.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Unit : MonoBehaviour
+{
+    public float maxHealth;
+    private float currentHealth;
+    public float defencePoint;
+    public Transform target;
+}
